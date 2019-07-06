7e84c865a8a9cefb2f084e1a501cfccafa7d15db
Slowl <hedi0475@gmail.com>
2019-07-06 22:04:31 +0200

    added style v1 of buttons

--- src/components/Requestor.jsx
+++ src/components/Requestor.jsx
@@ -2,18 +2,46 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Button = styled.div`
-  width: 60%;
-  padding: 2em;
+  display: flex;
+  justify-content: space-between;
+  min-width: 7%;
+  max-width: 30%;
+  padding: .8em 1.5em;
+  border-radius: 50px;
   background-color: ${props => props.request ? "#874444" : "#448755"};
   text-align: center;
   margin: auto;
+  cursor: pointer;
+  transition: .3s;
+
+  .icon {
+    font-size: 1.4em;
+    transition: .5s;
+    animation: ${props => props.loading ? "loading .5s ease infinite;" : ""};
+
+    @keyframes loading {
+      0% {transform: rotate(-0deg)}
+      100% {transform: rotate(-180deg)}
+    }
+  }
+
+  :hover {
+    .icon {
+      transform: ${props => props.request ? "rotate(-180deg)" : "translateX(6px)"};
+    }
+  }
+
+  :active {
+    .icon {
+      transform: ${props => props.request ? "rotate(0deg)" : "translateX(0px)"};
+    }
+  }
 `
 
-const Requestor = ({ onClick, request }) => {
-
+const Requestor = (Icons) => ({ onClick, request, rotator, children }) => {
   return (
-    <Button request={request} onClick={onClick}>
-      yes
+    <Button request={request} onClick={onClick} loading={rotator}>
+      {children} <Icons className="icon" />
     </Button>
   )
 }
