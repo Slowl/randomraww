407857e8e474dc3b694c94194b486de3392030ac
Slowl <hedi0475@gmail.com>
2019-07-06 17:52:57 +0200

    basic stylized title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const TitleContainer = styled.div`
+  max-width: 60%;
+  margin: 1em auto 1em;
+  padding: 1em;
+  background-color: #3b3b3b;
+  border-radius: 50px;
+  font-size: 1.4em;
+  letter-spacing: 1px;
+  text-align: center;
+
+  ::first-letter {
+    text-transform: uppercase;
+  }
+`
+
 const Title = ({ title }) => {
   return (
-    <div>{title}</div>
+    <TitleContainer>{title}</TitleContainer>
   )
 }
 
